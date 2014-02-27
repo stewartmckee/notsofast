73baf192ebe1459513cda187632adceba525df40
Stewart McKee <stewart@theizone.co.uk>
2014-02-27 14:57:31 +0000

    removed debug

--- lib/notsofast/rate_limit.rb
+++ lib/notsofast/rate_limit.rb
@@ -19,14 +19,12 @@ module Notsofast
       status, headers, response = @app.call(env)
 
       if timestamp - @connections_timestamp > Notsofast::Config.limit_expiry
-        puts "clearing connections cache"
         @connections_timestamp = timestamp
         @connections = {}
       end
 
       ip_address = remote_ip(env)
 
-      puts Notsofast::Config.whitelist.include?(ip_address)
       unless Notsofast::Config.whitelist.include?(ip_address)
         if Notsofast::Config.blacklist.include?(ip_address)
           response = Rack::Response.new
@@ -35,15 +33,12 @@ module Notsofast
           response.finish
           return
         else
-          puts "checking the content "
           if (headers["Content-Type"].nil? && Notsofast::Config.response_types.empty?) || (headers["Content-Type"].present? && Notsofast::Config.response_types.present? && Notsofast::Config.response_types.select{|r| headers["Content-Type"].include?(r)}.count > 0)
-            puts "matched content type"
             if @connections[ip_address]
               @connections[ip_address] += 1
             else
               @connections[ip_address] = 1
             end
-            puts @connections
 
             if @connections[ip_address] > Notsofast::Config.request_limit
               response = Rack::Response.new
